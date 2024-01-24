8a668f3ce53a4a585a6cbdba6bc3af03db20c4f0
Naitik Raj <user@gmail.com>
2024-01-24 10:39:30 +0530

    commit changes

--- hello.java/src/hello/java/Hello.java
+++ hello.java/src/hello/java/Hello.java
@@ -3,7 +3,6 @@ package hello.java;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.print("Naitik");
 		System.out.println(" Raj");
 	}
